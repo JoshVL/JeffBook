7ebe8391079c3957329e8f050dc6990eb2f171c0
Josh Villanueva <77131654+JoshVL@users.noreply.github.com>
2021-01-20 15:50:41 -0600

    Updated files from backup

---
+++ Person.java
@@ -0,0 +1,15 @@
+/**
+ * Person class to represent a person within JeffBook social network
+ *
+ */
+public class Person {
+  private String name;
+
+  /** Default constructor */
+  public Person() {}
+
+  /** Constructor given a name */
+  public Person(String name) {
+    this.name = name;
+  }
+}
